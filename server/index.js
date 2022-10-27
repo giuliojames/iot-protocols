1bdcb20b44fbc65157134efbb7ac05a48386aaf3
giuliojames <giuliojames.forlani@stud.itsaltoadriatico.it>
2022-10-27 11:48:32 +0200

    non so che cambiamento sia

--- server/index.js
+++ server/index.js
@@ -24,3 +24,18 @@ server.post('/drones/:id', function(req, res, next) {
 server.listen(8011, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
+const pgtools = require("pgtools");
+const config = {
+  user: "postgres",
+  host: "localhost",
+  password: "Belfast2022",
+  port: 5432
+};
+
+pgtools.(config, "myFirstDb", function(err, res) {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(res);
+});
